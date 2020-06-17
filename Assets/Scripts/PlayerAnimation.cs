620566bf58c73dafbeae5d4710366a320d0fa09d
Rodrigo Guizán Carballeira <hevachoProyectos@gmail.com>
2020-06-17 14:08:25 +0200

    animations

--- Assets/Scripts/PlayerAnimation.cs
+++ Assets/Scripts/PlayerAnimation.cs
@@ -6,16 +6,25 @@ public class PlayerAnimation : MonoBehaviour
 {
 
     private Animator _animator;
+    private Animator _swordAnimator;
 
     // Start is called before the first frame update
     void Start()
     {
         _animator = GetComponentInChildren<Animator>();
+        _swordAnimator = transform.GetChild(1).GetComponent<Animator>();
     }
 
-    public void Move(float move)
+    public void Move(float move, bool isGrounded)
     {
         _animator.SetFloat("Move", Mathf.Abs(move));
+        _animator.SetBool("Jumping", !isGrounded);
         
+    }
+
+    public void Attack()
+    {
+        _animator.SetTrigger("Attack");
+        _swordAnimator.SetTrigger("SwordAnimation");
     }
 }
